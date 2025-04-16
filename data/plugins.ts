f4a75b64f3f3a2f9b37aa4ba0c64bb67f5751f8f
D. Rimron-Soutter <darran@xalior.com>
2025-04-16 12:27:30 +0100

    Vibe coded Plugin architecture. Maybe bonkers ]. 🧠

---
+++ data/plugins.ts
@@ -0,0 +1,11 @@
+// import {ChannelFilterActions} from "../src/channel";
+//
+import {PingPlugin} from "../plugins/ping";
+import {ExamplePlugin} from "../plugins/exampleplugin";
+
+
+const plugins = [
+    ExamplePlugin,
+    PingPlugin
+];
+export { plugins };

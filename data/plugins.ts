a0196c4769a7aa677a6476a60332c7fe6c8016f3
D. Rimron-Soutter <darran@xalior.com>
2025-05-01 14:18:42 +0100

    Plugin loader / main loader now makes a bit more sense

--- data/plugins.ts
+++ data/plugins.ts
@@ -1,2 +1,6 @@
 // Define the plugin namespaces to load - this is just a js array
-export const plugins = ['org.xalior.ping', 'org.xalior.example', 'org.xalior.commands'];
+export const plugins = [
+    'org.xalior.ping',
+    'org.xalior.example',
+    'org.xalior.commands'
+];
--- src/bot.ts
+++ src/bot.ts
@@ -2,17 +2,57 @@
 import { env } from "./env"
 // @ts-ignore
 import { guilds } from "../data/guilds.js"
-import { GatewayIntentBits, Message, OmitPartialGroupDMChannel, TextChannel} from 'discord.js';
+import { Message, OmitPartialGroupDMChannel, TextChannel} from 'discord.js';
 import { client, DiscordMessage } from './discord';
-import { GuildData } from "./guild";
 import { filter } from "./filters";
 import {action} from "./actions";
-import {load_plugins, plugins} from "./plugin";
+import {load_plugins,} from "./plugin";
 import express from 'express';
-
-let client_id: string | undefined;
 
 const app = express();
 app.set('trust proxy', 1);
 
 load_plugins(app);
+
+// The bot basically functions around incoming messages, which we start from here...
+client.on('messageCreate', async (discord_message:OmitPartialGroupDMChannel<Message>) => {
+    // Don't respond to automated messages
+    if (discord_message.author.bot) return;
+
+    // Filter message by server (guild)
+    if(guilds[discord_message.guildId]) {
+
+        // Create our own state-managed version of the incoming message
+        const incoming = new DiscordMessage(client, guilds[discord_message.guildId], discord_message);
+
+        // Per Channel
+        for (const guild_channel of incoming.guild_data.channels) {
+            // Correct channel?
+            if (incoming.message.channelId === guild_channel.channel_id ||
+                (guild_channel.channel_id === '*')) {
+
+                // Required Terms
+                let required_filters = filter(incoming, guild_channel.filters?.required);
+
+                // Banned Terms
+                let banned_filters = filter(incoming, guild_channel.filters?.banned);
+
+                // Anything can fail in the action, to we best wrap it...
+                try {
+                    // All filtering done, now deal with results - which filters depends on the results of both sets...
+                    await action(incoming, (guild_channel.filters?.all ||  (required_filters && !banned_filters)) ? guild_channel.pass : guild_channel.fail);
+                }
+                catch (error: any) {
+                    try {
+                        await (client.channels.cache.get(incoming.guild_data.log_channel_id) as TextChannel).send(error as string);
+                    }
+                    finally {
+                        console.error(`ERROR: ${error} on ${incoming.message.guildId}.${incoming.message.channelId}:{"${incoming.message.content}"}`);
+                    }
+                }
+            }
+        }
+    }
+});
+
+client.login(env.BOT_TOKEN);
